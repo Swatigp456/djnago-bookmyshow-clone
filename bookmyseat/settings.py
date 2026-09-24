8ba9acf42aaf95b8689885127a9c61d31de7cfe7
Swati Potdar <swatipoddar36@gmail.com>
2026-09-24 21:31:11 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-build-time-key-replace-me')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
